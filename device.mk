38cab3bd1c1f921aa3b47a573efef85c87f54713
Abdalrahman Eissa <abdalrahmaneissaa@gmail.com>
2024-08-16 23:29:12 +0300

    Add overlay-lineage Path in device.mk

--- device.mk
+++ device.mk
@@ -30,7 +30,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
