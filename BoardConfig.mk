2c08f4966ece4755844e8942bec069775ab92a51
Abdalrahman Eissa <abdalrahmaneissaa@gmail.com>
2024-09-20 02:46:38 +0300

    Rename fstab.default To fstab.recvoery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.default
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # User Image Partitions
