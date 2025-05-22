4b54b26e7ee77e84d1c24ded22565055bd61795d
Abdalrahman Eissa <abdalrahmaneissaa@proton.me>
2025-05-22 20:10:32 +0300

    BoardConfig.mk: Change kernel CMDLINEs to compatible with vantom kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,24 +30,18 @@ ODM_MANIFEST_FILES := $(DEVICE_PATH)/VINTF/manifest-nfc.xml
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 
 TARGET_KERNEL_CONFIG := courbet_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/courbet
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r522817
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
-BOARD_KERNEL_CMDLINE := androidboot.memcg=1
-BOARD_KERNEL_CMDLINE := lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000
-BOARD_KERNEL_CMDLINE := msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE := service_locator.enable=1
-BOARD_KERNEL_CMDLINE := swiotlb=1
-BOARD_KERNEL_CMDLINE := androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE := loop.max_part=7
-BOARD_KERNEL_CMDLINE := buildvariant=user
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
